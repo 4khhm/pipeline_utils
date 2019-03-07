14ef59ce129d1d2fd3a89d799e4a6312e7155139
pete lunghino <petelunghino@gmail.com>
2019-03-06 17:35:42 -0800

    initial commit

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -4,14 +4,14 @@ import numpy as np
 
 # finds the binary features of a dataframe and creates a list of their names
 
-def findBinary(df):
+def find_binary(df):
 	lst = []
 	for column in df:
 		if (df[column].value_counts()).size==2:
 			lst.append(column)
 	return lst
 
-def makeBoolian(df,lst):
+def make_boolian(df,lst):
 	values = []
 	for column in df[lst]:	
 		series = df[column].value_counts()
@@ -23,38 +23,37 @@ def makeBoolian(df,lst):
 		df[header].replace([option1,option0], [1,0], inplace = True)
 	return df
 
-def addNaNData(df):
+def add_nan_data(df):
 	noNaN = []
 	# get dataframe boolian of nan values
 	whereNaN = df.isnull()# df
 	# get feature and example wise nan totals
 	featureTotalNaN = pd.DataFrame(whereNaN.sum(axis=0).rename("FT_NUMBER_NAN")) # df transpose
 	exampleTotalNaN = whereNaN.sum(axis=1).rename("EX_NUMBER_NAN") # series
-	# add ex sums to dataframe (row first or error)
 	df = pd.concat([df,exampleTotalNaN], axis=1) # add as column
 	for column in featureTotalNaN:
 	    if featureTotalNaN.iloc[0][column]==0:
 	        noNaN.append(column)
-	return (noNaN, df)
+	return noNaN, df
 
-def findNoNaN(df):
+def find_nonan(df):
 	lst = []
 	for column in df:
 		if (df[column].value_counts()).size==2:
 			lst.append(column)
 	return binaryFeatures
 
-def findCategorical(df):
+def find_categorical_discrete(df, threshold=20):
 	lst=[]
 	for column in df:
-		if (df[column].value_counts()).size<30:
+		if (df[column].value_counts()).size<threshold:
 			if (df[column].value_counts()).size>2:
 				lst.append(column)
 	return lst, df
 
-def findNan(df):
+def findnan(df):
 	whereNan = df.isnull()#df
 	countNan = df_whereNan.sum(axis=0) #seies
 	pd.concat([df,numberNan])
-	return (df_whereNaN, df_numberNaN)
+	return df_whereNaN, df_numberNaN
  
